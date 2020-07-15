1de7d6da981ba8dbd122747e4f41a36da89fc1a4
github user ryo-wens <ryousuke.11460@gmail.com>
2020-07-16 00:09:19 +0900

    Modify input form

--- src/components/uikit/CategoryInput.tsx
+++ src/components/uikit/CategoryInput.tsx
@@ -8,37 +8,49 @@ import Select from '@material-ui/core/Select';
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     formControl: {
+      marginTop: 0,
+      marginBottom: 16,
       margin: theme.spacing(1),
       minWidth: 225,
     },
   })
 );
 
-type CategoryProps = {
+interface CategoryProps {
   value: string;
   onChange: (event: React.ChangeEvent<{ value: unknown }>) => void;
   required: boolean;
-};
+}
 
 const CategoryInput = (props: CategoryProps) => {
   const classes = useStyles();
 
   return (
-    <FormControl className={classes.formControl}>
-      <InputLabel id="category" required={props.required}>
-        カテゴリー
-      </InputLabel>
-      <Select value={props.value} onChange={props.onChange}>
-        <MenuItem value=""></MenuItem>
-        <MenuItem value={'食費'}>食費</MenuItem>
-        <MenuItem value={'日用雑貨品'}>日用雑貨品</MenuItem>
-        <MenuItem value={'交通費'}>交通費</MenuItem>
-        <MenuItem value={'医療費'}>医療費</MenuItem>
-        <MenuItem value={'水道、光熱費'}>水道、光熱費</MenuItem>
-        <MenuItem value={'家賃、住宅ローン'}>家賃、住宅ローン</MenuItem>
-        <MenuItem value={'その他'}>その他</MenuItem>
-      </Select>
-    </FormControl>
+    <>
+      <FormControl className={classes.formControl}>
+        <InputLabel id="category" required={props.required}>
+          カテゴリー
+        </InputLabel>
+        <Select value={props.value} onChange={props.onChange}>
+          <MenuItem value=""></MenuItem>
+          <MenuItem value={'食費'}>食費</MenuItem>
+          <MenuItem value={'日用品'}>日用品</MenuItem>
+          <MenuItem value={'趣味・娯楽'}>趣味・娯楽</MenuItem>
+          <MenuItem value={'交際費'}>交際費</MenuItem>
+          <MenuItem value={'交通費'}>交通費</MenuItem>
+          <MenuItem value={'衣服・美容'}>衣服・美容</MenuItem>
+          <MenuItem value={'健康・医療'}>健康・医療</MenuItem>
+          <MenuItem value={'通信費'}>通信費</MenuItem>
+          <MenuItem value={'住宅'}>住宅</MenuItem>
+          <MenuItem value={'水道・光熱費'}>水道・光熱費</MenuItem>
+          <MenuItem value={'自動車'}>自動車</MenuItem>
+          <MenuItem value={'保険'}>保険</MenuItem>
+          <MenuItem value={'税金・社会保険'}>税金・社会保険</MenuItem>
+          <MenuItem value={'現金・カード'}>現金・カード</MenuItem>
+          <MenuItem value={'その他'}>その他</MenuItem>
+        </Select>
+      </FormControl>
+    </>
   );
 };
 export default CategoryInput;
