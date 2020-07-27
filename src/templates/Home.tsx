a2acc426c7ac75091aef868b0b8a63f7f1b9366c
github user ryo-wens <ryousuke.11460@gmail.com>
2020-07-27 17:29:33 +0900

    Add monthly conditions and expense style

--- src/templates/Home.tsx
+++ src/templates/Home.tsx
@@ -2,6 +2,13 @@ import React from 'react';
 import { InputForm } from '../components/home';
 
 const Home = () => {
-  return <InputForm />;
+  return (
+    <>
+      <InputForm />
+      <div className="box__monthly" />
+      <div className="box__todo" />
+      <div className="box__monthlyexpense" />
+    </>
+  );
 };
 export default Home;
