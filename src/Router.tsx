c55abf3d45e59f3556dc0aaa9dd62dccd2da136d
github user ryo-wens <ryousuke.11460@gmail.com>
2020-08-12 17:11:59 +0900

    Add selectBigCategory template

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import { Route, Switch } from 'react-router';
-import { Todo, SignUp, LogIn, Home } from './templates';
+import { Todo, SignUp, LogIn, Home, SelectBigCategory } from './templates';
 
-const Router = () => {
+const Router = (): JSX.Element => {
   return (
     <Switch>
       <Route exact path={'/todo'} component={Todo} />
       <Route exact path={'/signup'} component={SignUp} />
       <Route exact path={'/login'} component={LogIn} />
       <Route exact path={'/'} component={Home} />
+      <Route exact path={'/big-categories'} component={SelectBigCategory} />
+      <Route exact path={'/custom-categories/:id'} component={SelectBigCategory} />
     </Switch>
   );
 };
