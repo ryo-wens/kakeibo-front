771d6eb2e38523bac743e5f5f8f86a77c7211548
github user ryo-wens <ryousuke.11460@gmail.com>
2020-08-15 19:31:07 +0900

    Add todo

---
+++ src/components/todo/TaskTextFields.tsx
@@ -0,0 +1,46 @@
+import React, { useState, useCallback } from 'react';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import { TodoButton } from './index';
+import Box from '@material-ui/core/Box';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    textarea: {
+      '& > *': {
+        margin: theme.spacing(1),
+        width: '60ch',
+      },
+    },
+    box: {
+      width: '60ch',
+      borderColor: '#ccc',
+    },
+    buttons: {
+      display: 'flex',
+    },
+  })
+);
+
+const TaskTextFields = () => {
+  const classes = useStyles();
+
+  return (
+    <form noValidate autoComplete="off">
+      <Box border={1} className={classes.box}>
+        <TextField className={classes.textarea} id="filled-basic" variant="filled" />
+        <div className={classes.buttons} style={{ justifyContent: 'flex-end' }}>
+          <TodoButton label={'画像をアップロード'} onClick={() => console.log('クリック')} />
+          <TodoButton label={'期限'} onClick={() => console.log('クリック')} />
+        </div>
+      </Box>
+
+      <div className={classes.buttons}>
+        <TodoButton label={'タスクを追加'} onClick={() => console.log('クリック')} />
+        <TodoButton label={'キャンセル'} onClick={() => console.log('クリック')} />
+      </div>
+    </form>
+  );
+};
+
+export default TaskTextFields;
