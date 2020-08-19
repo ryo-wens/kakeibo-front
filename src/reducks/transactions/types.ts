bd8258fc3aadd846e352f6b915b9c1f97fa920a9
github user ryo-wens <ryousuke.11460@gmail.com>
2020-08-19 18:27:55 +0900

    Add the types and actions of transactions

---
+++ src/reducks/transactions/types.ts
@@ -0,0 +1,10 @@
+export interface Transactions {
+  transaction_type:string,
+  transaction_date:Date,
+  shop:string,
+  memo:string,
+  amount:number,
+  big_category_id:number,
+  medium_category_id:number,
+  custom_category_id:number
+}
