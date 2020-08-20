2d6c6cb698172970f81d756f0fc48971a32ae690
github user ryo-wens <ryousuke.11460@gmail.com>
2020-08-21 02:47:30 +0900

    Add the AddTransactions type to the transaction

--- src/reducks/transactions/types.ts
+++ src/reducks/transactions/types.ts
@@ -1,4 +1,4 @@
-export interface Transaction {
+export interface GetTransaction {
   id:number,
   transaction_type:string,
   updated_date:Date,
@@ -11,4 +11,14 @@ export interface Transaction {
   custom_category_id?:number
 }
 
-export interface Transactions extends Array<Transaction>{}
+export interface AddTransactions {
+  transaction_type: string,
+  transaction_date: Date,
+  shop?: string,
+  mem?: string,
+  amount: number,
+  big_category_id: number,
+  medium_category_id?: number,
+  custom_category_id?: number
+}
+export interface Transactions extends Array<GetTransaction>{}
