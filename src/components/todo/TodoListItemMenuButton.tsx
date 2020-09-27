6131073aed01a601412cd75122cc0b68592f580e
Hiroya Furusawa <pogbba.6@gmail.com>
2020-09-27 21:02:52 +0900

    Create todoListItemMenuButton.tsx

---
+++ src/components/todo/TodoListItemMenuButton.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
+
+const TodoListItemMenuButton = () => {
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <>
+      <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
+        <MoreHorizIcon />
+      </Button>
+      <Menu
+        id="simple-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <MenuItem onClick={handleClose}>Todoを編集</MenuItem>
+        <MenuItem onClick={handleClose}>Todoを削除</MenuItem>
+      </Menu>
+    </>
+  );
+};
+
+export default TodoListItemMenuButton;
