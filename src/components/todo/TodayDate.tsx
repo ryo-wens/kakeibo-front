5e43bf730431ed0e956c554aa5416b013ca99ab6
Hiroya Furusawa <pogbba.6@gmail.com>
2020-09-29 15:43:52 +0900

    Add fetchDateTodoLists in todoLists/operations.ts

--- src/components/todo/TodayDate.tsx
+++ src/components/todo/TodayDate.tsx
@@ -4,15 +4,15 @@ import { AddTodo, TodoListItem } from './index';
 const TodayDate = () => {
   const dt: Date = new Date();
   const month = dt.getMonth() + 1;
-  const day = dt.getDate();
-  const dateT = ['日', '月', '火', '水', '木', '金', '土'];
-  const date = dateT[dt.getDay()];
+  const date = dt.getDate();
+  const dayT = ['日', '月', '火', '水', '木', '金', '土'];
+  const day = dayT[dt.getDay()];
   const groupId = 0;
 
   return (
     <>
       <span>
-        今日 {month}/{day} ({date})
+        今日 {month}/{date} ({day})
       </span>
       <TodoListItem />
       <div>
