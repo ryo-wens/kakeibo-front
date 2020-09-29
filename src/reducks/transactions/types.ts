c0329a68b4a828d08009f3f6020647af4512407e
github user ryo-wens <ryousuke.11460@gmail.com>
2020-09-29 17:50:03 +0900

    Add the operations of fetchTransactionsList

--- src/reducks/transactions/types.ts
+++ src/reducks/transactions/types.ts
@@ -22,4 +22,37 @@ export interface AddTransactions {
   custom_category_id?: number | null;
 }
 
+export interface fetchTransactionsRes {
+  message: string;
+  transactions_list: [];
+}
+
+export interface transactionsReq {
+  transaction_type: string;
+  transaction_date: Date | null;
+  shop: string | null;
+  memo: string | null;
+  amount: string | number;
+  big_category_id: number;
+  medium_category_id: number | null;
+  custom_category_id: number | null;
+}
+
+export interface transactionsRes {
+  id: number;
+  transaction_type: string;
+  updated_date: Date;
+  transaction_date: string;
+  shop: string | null;
+  memo: string | null;
+  amount: number;
+  big_category_name: string;
+  medium_category_name: string | null;
+  custom_category_name: string | null;
+}
+
+export interface deleteTransactionRes {
+  message: string;
+}
+
 export interface TransactionsList extends Array<FetchTransactions> {}
