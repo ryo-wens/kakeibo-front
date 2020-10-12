424e781e08b4490a4a54e726e1ea7d472127dfc3
Hiroya Furusawa <pogbba.6@gmail.com>
2020-10-12 13:43:48 +0900

    Add FETCH_GROUP_MONTH_TODO_LISTS in actions.ts and reducers.ts

--- src/reducks/groupTodoLists/actions.ts
+++ src/reducks/groupTodoLists/actions.ts
@@ -1,6 +1,8 @@
 import { GroupTodoLists } from './types';
 export type groupTodoListsActions = ReturnType<
-  typeof createGroupTodoListItemAction | typeof fetchGroupDateTodoListsAction
+  | typeof createGroupTodoListItemAction
+  | typeof fetchGroupDateTodoListsAction
+  | typeof fetchGroupMonthTodoListsAction
 >;
 
 export const CREATE_GROUP_TODO_LIST_ITEM = 'CREATE_GROUP_TODO_LIST_ITEM';
@@ -19,6 +21,22 @@ export const createGroupTodoListItemAction = (
 
 export const FETCH_GROUP_DATE_TODO_LISTS = 'FETCH_GROUP_DATE_TODO_LISTS';
 export const fetchGroupDateTodoListsAction = (
+  groupImplementationTodoLists: GroupTodoLists,
+  groupDueTodoLists: GroupTodoLists,
+  message: string
+) => {
+  return {
+    type: FETCH_GROUP_DATE_TODO_LISTS,
+    payload: {
+      groupImplementationTodoLists: groupImplementationTodoLists,
+      groupDueTodoLists: groupDueTodoLists,
+      message: message,
+    },
+  };
+};
+
+export const FETCH_GROUP_MONTH_TODO_LISTS = 'FETCH_GROUP_MONTH_TODO_LISTS';
+export const fetchGroupMonthTodoListsAction = (
   groupImplementationTodoLists: GroupTodoLists,
   groupDueTodoLists: GroupTodoLists,
   message: string
