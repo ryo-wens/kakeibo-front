cc6037c992ece8c0b72d293206366880ea8874d0
github user ryo-wens <ryousuke.11460@gmail.com>
2021-02-11 11:51:02 +0900

    Modify operations for groupCategories

--- src/reducks/groupCategories/operations.ts
+++ src/reducks/groupCategories/operations.ts
@@ -1,222 +1,169 @@
-import { updateGroupIncomeCategoriesAction, updateGroupExpenseCategoriesAction } from './actions';
+import {
+  fetchGroupIncomeCategoriesAction,
+  fetchGroupExpenseCategoriesAction,
+  addGroupIncomeCategory,
+  addGroupExpenseCategory,
+  editGroupIncomeCategory,
+  editGroupExpenseCategory,
+  deleteIncomeCategory,
+  deleteExpenseCategory,
+} from './actions';
 import axios, { CancelTokenSource } from 'axios';
 import { Dispatch, Action } from 'redux';
 import {
   fetchGroupCategoriesRes,
-  GroupCategory,
   operationCategoriesReq,
   operationCategoriesRes,
   deleteGroupCustomCategoriesRes,
 } from './types';
 import { errorHandling } from '../../lib/validation';
-import { State } from '../store/types';
 
 export const fetchGroupCategories = (groupId: number, signal: CancelTokenSource) => {
   return async (dispatch: Dispatch<Action>): Promise<void> => {
-    await axios
-      .get<fetchGroupCategoriesRes>(
+    try {
+      const fetchResult = await axios.get<fetchGroupCategoriesRes>(
         `${process.env.REACT_APP_ACCOUNT_API_HOST}/groups/${groupId}/categories`,
         {
           cancelToken: signal.token,
           withCredentials: true,
         }
-      )
-      .then((res) => {
-        const groupIncomeCategories = res.data.income_categories_list;
-        const groupExpenseCategories = res.data.expense_categories_list;
-        dispatch(updateGroupIncomeCategoriesAction(groupIncomeCategories));
-        dispatch(updateGroupExpenseCategoriesAction(groupExpenseCategories));
-      })
-      .catch((error) => {
-        if (axios.isCancel(error)) {
-          return;
-        } else {
-          errorHandling(dispatch, error);
-        }
-      });
+      );
+      const groupIncomeCategories = fetchResult.data.income_categories_list;
+      const groupExpenseCategories = fetchResult.data.expense_categories_list;
+
+      dispatch(fetchGroupIncomeCategoriesAction(groupIncomeCategories));
+      dispatch(fetchGroupExpenseCategoriesAction(groupExpenseCategories));
+    } catch (error) {
+      if (axios.isCancel(error)) {
+        return;
+      } else {
+        errorHandling(dispatch, error);
+      }
+    }
   };
 };
 
-export const addGroupCustomCategories = (name: string, bigCategoryId: number) => {
-  return async (dispatch: Dispatch<Action>, getState: () => State): Promise<void> => {
+export const addGroupCustomCategories = (
+  name: string,
+  bigCategoryId: number,
+  groupId: number,
+  signal: CancelTokenSource
+) => {
+  return async (dispatch: Dispatch<Action>): Promise<void> => {
     const data: operationCategoriesReq = {
       name: name,
       big_category_id: bigCategoryId,
     };
-    await axios
-      .post<operationCategoriesRes>(
-        `${process.env.REACT_APP_ACCOUNT_API_HOST}/groups/1/categories/custom-categories`,
+    try {
+      await axios.post<operationCategoriesRes>(
+        `${process.env.REACT_APP_ACCOUNT_API_HOST}/groups/${groupId}/categories/custom-categories`,
         JSON.stringify(data),
         {
           withCredentials: true,
         }
-      )
-      .then((res) => {
-        const addedCategory = res.data;
+      );
 
-        // bigCategoryId = 1 収入 : bigCategoryId > 1 支出
-        if (bigCategoryId === 1) {
-          const incomeGroupCategories = getState().groupCategories.groupIncomeList;
-
-          const nextGroupIncomeCategories = incomeGroupCategories.map(
-            (incomeGroupCategory: GroupCategory) => {
-              if (incomeGroupCategory.id === bigCategoryId) {
-                const prevAssociatedIncomeCategories =
-                  incomeGroupCategory.associated_categories_list;
-                incomeGroupCategory.associated_categories_list = [
-                  addedCategory,
-                  ...prevAssociatedIncomeCategories,
-                ];
-              }
-              return incomeGroupCategory;
-            }
-          );
-          dispatch(updateGroupIncomeCategoriesAction(nextGroupIncomeCategories));
-        } else {
-          const expenseGroupCategories = getState().groupCategories.groupExpenseList;
-
-          const nextGroupExpenseCategories = expenseGroupCategories.map(
-            (expenseGroupCategory: GroupCategory) => {
-              if (expenseGroupCategory.id === bigCategoryId) {
-                const prevAssociatedExpenseCategories =
-                  expenseGroupCategory.associated_categories_list;
-                expenseGroupCategory.associated_categories_list = [
-                  addedCategory,
-                  ...prevAssociatedExpenseCategories,
-                ];
-              }
-              return expenseGroupCategory;
-            }
-          );
-          dispatch(updateGroupExpenseCategoriesAction(nextGroupExpenseCategories));
+      const fetchResult = await axios.get<fetchGroupCategoriesRes>(
+        `${process.env.REACT_APP_ACCOUNT_API_HOST}/groups/${groupId}/categories`,
+        {
+          cancelToken: signal.token,
+          withCredentials: true,
         }
-      })
-      .catch((error) => {
-        errorHandling(dispatch, error);
-      });
+      );
+
+      const addedGroupIncomeCategoriesList = fetchResult.data.income_categories_list;
+      const addedGroupExpenseCategoriesList = fetchResult.data.expense_categories_list;
+
+      // bigCategoryId = 1 収入 : bigCategoryId > 1 支出
+      if (bigCategoryId === 1) {
+        dispatch(addGroupIncomeCategory(addedGroupIncomeCategoriesList));
+      } else {
+        dispatch(addGroupExpenseCategory(addedGroupExpenseCategoriesList));
+      }
+    } catch (error) {
+      errorHandling(dispatch, error);
+    }
   };
 };
 
-export const editGroupCustomCategories = (id: number, name: string, bigCategoryId: number) => {
-  return async (dispatch: Dispatch<Action>, getState: () => State): Promise<void> => {
+export const editGroupCustomCategories = (
+  id: number,
+  name: string,
+  bigCategoryId: number,
+  groupId: number,
+  signal: CancelTokenSource
+) => {
+  return async (dispatch: Dispatch<Action>): Promise<void> => {
     const data: operationCategoriesReq = {
       name: name,
       big_category_id: bigCategoryId,
     };
-    await axios
-      .put<operationCategoriesRes>(
-        `${process.env.REACT_APP_ACCOUNT_API_HOST}/groups/1/categories/custom-categories/${id}`,
+    try {
+      await axios.put<operationCategoriesRes>(
+        `${process.env.REACT_APP_ACCOUNT_API_HOST}/groups/${groupId}/categories/custom-categories/${id}`,
         JSON.stringify(data),
         {
           withCredentials: true,
         }
-      )
-      .then((res) => {
-        const editedCategory = res.data;
+      );
 
-        // bigCategoryId = 1 収入 : bigCategoryId > 1 支出
-        if (bigCategoryId === 1) {
-          const groupIncomeCategories = getState().groupCategories.groupIncomeList;
-
-          const nextGroupIncomeCategories = groupIncomeCategories.map(
-            (incomeGroupCategory: GroupCategory) => {
-              if (incomeGroupCategory.id === bigCategoryId) {
-                const prevAssociatedCategories = incomeGroupCategory.associated_categories_list;
-                incomeGroupCategory.associated_categories_list = prevAssociatedCategories.map(
-                  (associatedCategory) => {
-                    if (
-                      associatedCategory.id === id &&
-                      associatedCategory.category_type === 'CustomCategory'
-                    ) {
-                      return editedCategory;
-                    }
-                    return associatedCategory;
-                  }
-                );
-              }
-              return incomeGroupCategory;
-            }
-          );
-          dispatch(updateGroupIncomeCategoriesAction(nextGroupIncomeCategories));
-        } else {
-          const groupExpenseCategories = getState().groupCategories.groupExpenseList;
-
-          const nextGroupExpenseCategories = groupExpenseCategories.map(
-            (expenseGroupCategory: GroupCategory) => {
-              if (expenseGroupCategory.id === bigCategoryId) {
-                const prevAssociatedCategories = expenseGroupCategory.associated_categories_list;
-                expenseGroupCategory.associated_categories_list = prevAssociatedCategories.map(
-                  (associatedCategory) => {
-                    if (
-                      associatedCategory.id === id &&
-                      associatedCategory.category_type === 'CustomCategory'
-                    ) {
-                      return editedCategory;
-                    }
-                    return associatedCategory;
-                  }
-                );
-              }
-              return expenseGroupCategory;
-            }
-          );
-          dispatch(updateGroupExpenseCategoriesAction(nextGroupExpenseCategories));
+      const fetchResult = await axios.get<fetchGroupCategoriesRes>(
+        `${process.env.REACT_APP_ACCOUNT_API_HOST}/groups/${groupId}/categories`,
+        {
+          cancelToken: signal.token,
+          withCredentials: true,
         }
-      })
-      .catch((error) => {
-        errorHandling(dispatch, error);
-      });
+      );
+
+      const editedGroupIncomeCategories = fetchResult.data.income_categories_list;
+      const editedGroupExpenseCategories = fetchResult.data.expense_categories_list;
+
+      // bigCategoryId = 1 収入 : bigCategoryId > 1 支出
+      if (bigCategoryId === 1) {
+        dispatch(editGroupIncomeCategory(editedGroupIncomeCategories));
+      } else {
+        dispatch(editGroupExpenseCategory(editedGroupExpenseCategories));
+      }
+    } catch (error) {
+      errorHandling(dispatch, error);
+    }
   };
 };
 
-export const deleteGroupCustomCategories = (id: number, bigCategoryId: number) => {
-  return async (dispatch: Dispatch<Action>, getState: () => State): Promise<void> => {
-    await axios
-      .delete<deleteGroupCustomCategoriesRes>(
-        `${process.env.REACT_APP_ACCOUNT_API_HOST}/groups/1/categories/custom-categories/${id}`,
+export const deleteGroupCustomCategories = (
+  id: number,
+  bigCategoryId: number,
+  groupId: number,
+  signal: CancelTokenSource
+) => {
+  return async (dispatch: Dispatch<Action>): Promise<void> => {
+    try {
+      await axios.delete<deleteGroupCustomCategoriesRes>(
+        `${process.env.REACT_APP_ACCOUNT_API_HOST}/groups/${groupId}/categories/custom-categories/${id}`,
         {
           withCredentials: true,
         }
-      )
-      .then((res) => {
-        const resMessage = res.data.message;
+      );
 
-        // bigCategoryId = 1 収入 : bigCategoryId > 1 支出
-        if (bigCategoryId === 1) {
-          const incomeGroupCategories = getState().groupCategories.groupIncomeList;
-          const nextGroupIncomeCategories = incomeGroupCategories.map(
-            (incomeGroupCategory: GroupCategory) => {
-              if (incomeGroupCategory.id === bigCategoryId) {
-                const prevAssociatedCategories = incomeGroupCategory.associated_categories_list;
-                incomeGroupCategory.associated_categories_list = prevAssociatedCategories.filter(
-                  (associatedCategory) => associatedCategory.id !== id
-                );
-              }
-              return incomeGroupCategory;
-            }
-          );
-          alert(resMessage);
-          dispatch(updateGroupIncomeCategoriesAction(nextGroupIncomeCategories));
-        } else {
-          const expenseGroupCategories = getState().groupCategories.groupExpenseList;
-
-          const nextGroupExpenseCategories = expenseGroupCategories.map(
-            (expenseGroupCategory: GroupCategory) => {
-              if (expenseGroupCategory.id === bigCategoryId) {
-                const prevAssociatedCategories = expenseGroupCategory.associated_categories_list;
-                expenseGroupCategory.associated_categories_list = prevAssociatedCategories.filter(
-                  (associatedCategory) => associatedCategory.id !== id
-                );
-              }
-              return expenseGroupCategory;
-            }
-          );
-          alert(resMessage);
-          dispatch(updateGroupExpenseCategoriesAction(nextGroupExpenseCategories));
+      const fetchResult = await axios.get<fetchGroupCategoriesRes>(
+        `${process.env.REACT_APP_ACCOUNT_API_HOST}/groups/${groupId}/categories`,
+        {
+          cancelToken: signal.token,
+          withCredentials: true,
         }
-      })
-      .catch((error) => {
-        errorHandling(dispatch, error);
-      });
+      );
+
+      const deletedGroupIncomeCategories = fetchResult.data.income_categories_list;
+      const deletedGroupExpenseCategories = fetchResult.data.expense_categories_list;
+
+      // bigCategoryId = 1 収入 : bigCategoryId > 1 支出
+      if (bigCategoryId === 1) {
+        dispatch(deleteIncomeCategory(deletedGroupIncomeCategories));
+      } else {
+        dispatch(deleteExpenseCategory(deletedGroupExpenseCategories));
+      }
+    } catch (error) {
+      errorHandling(dispatch, error);
+    }
   };
 };
