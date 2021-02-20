b240d769834cdeb2026813500e07fbd68d540ca6
github user ryo-wens <ryousuke.11460@gmail.com>
2021-02-20 16:05:47 +0900

    Delete userName and email from users selectors

--- src/reducks/users/selectors.ts
+++ src/reducks/users/selectors.ts
@@ -4,7 +4,5 @@ import { State } from '../store/types';
 const usersSelector = (state: State) => state.users;
 
 export const getUserId = createSelector([usersSelector], (state) => state.user_id);
-export const getUserName = createSelector([usersSelector], (state) => state.name);
-export const getEmail = createSelector([usersSelector], (state) => state.email);
 export const getErrorMessage = createSelector([usersSelector], (state) => state.errorMessage);
 export const getConflictMessage = createSelector([usersSelector], (state) => state.conflictMessage);
